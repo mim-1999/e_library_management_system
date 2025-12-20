af8533fa7a202070fe9cb71dabdcf6fb36408937
mim-1999 <tasnimkhondoker1999@gmail.com>
2025-12-19 22:31:27 -0500

    New feature

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+  # def _hash_password(self, plain_password):
+    #     """Hash a password for security"""
+    #     salt = bcrypt.gensalt()
+    #     hashed = bcrypt.hashpw(plain_password.encode(), salt)
+    #     return hashed.decode()  # Convert bytes to string for database
